dd43c0f906998cdd092536cafeb271fa2e58298f
aleksamarkoni <aleksamarkoni@gmail.com>
2019-01-14 13:34:55 +0100

    dodao neka resenja

--- 10267 - Graphical Editor/main.cpp
+++ 10267 - Graphical Editor/main.cpp
@@ -10,6 +10,10 @@ int main() {
     char c;
     int m, n;
     int x, y;
+    int y1, y2;
+    int x1, x2;
+    int red;
+    int kolona;
     char color;
     string fileName;
     for (int i = 0; i < 250; i++) {
@@ -20,19 +24,65 @@ int main() {
     bool running = true;
     while (running) {
         cin >> c;
-        switch(c) {
+        switch (c) {
             case 'I':
-                cin >> n >> m;
+                cin >> m >> n;
+                for (int i = 0; i < m; i++) {
+                    for (int j = 0; j < n; j++) {
+                        mat[i][j] = '0';
+                    }
+                }
                 break;
             case 'L':
                 cin >> x >> y >> color;
                 mat[y][x] = color;
                 break;
             case 'C':
+                for (int i = 0; i < m; i++) {
+                    for (int j = 0; j < n; j++) {
+                        mat[i][j] = '0';
+                    }
+                }
+                break;
+            case 'V':
+                cin >> kolona >> y1 >> y2 >> color;
+                for (int i = 0; i < m; i++) {
+                    for (int j = 0; j < n; j++) {
+                        if (i == kolona && j >= y1 && j <= y2) {
+                            mat[j - 1][kolona - 1] = color;
+                        }
+                    }
+                }
+                break;
+            case 'H':
+                cin >> x1 >> x2 >> red >> color;
+                for (int i = 0; i < m; i++) {
+                    for (int j = 0; j < n; j++) {
+                        if (i >= x1 && i <= x2 && j == red) {
+                            mat[red - 1][i - 1] = color;
+                        } else if (j <= x2 && j >= y2) {
+                        }
+                    }
+                }
+                break;
+            case 'K':
+                cin >> x1 >> y1 >> x2 >> y2 >> color;
+                for (int i = 0; i < m; i++) {
+                    for (int j = 0; j < n; j++) {
+
+                    }
+                }
+
                 break;
             case 'S':
                 cin >> fileName;
                 cout << fileName << endl;
+                for (int i = 0; i < m; i++) {
+                    for (int j = 0; j < n; j++) {
+                        cout << mat[i][j];
+                    }
+                    cout << endl;
+                }
                 //todo odstampati matricu
                 break;
             case 'X':
